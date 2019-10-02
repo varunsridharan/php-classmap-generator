e351000c1afcd8f6879e3c891f399d465982597a
Varun <varunsridharan23@gmail.com>
2019-10-02 12:02:51 +0530

    minor bug fixed.

--- src/generator.php
+++ src/generator.php
@@ -232,8 +232,8 @@ class ClassMapGenerator {
 
 if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/../vendor/autoload.php';
-} elseif ( file_exists( __DIR__ . '/../../../autoload.php' ) ) {
-	require_once __DIR__ . '/../../../vendor/autoload.php';
+} elseif ( file_exists( dirname( dirname( dirname( __DIR__ ) ) ) . '/autoload.php' ) ) {
+	require_once dirname( dirname( dirname( __DIR__ ) ) ) . '/autoload.php';
 }
 
 $config_file = ( isset( $argv[1] ) ) ? $argv[1] : false;
